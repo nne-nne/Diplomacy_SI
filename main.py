ae441737aa0a5f61c1c431b553cfac642e7a169c
nne-nne <adam.leczkowski1@gmail.com>
2021-04-15 12:40:37 +0200

    map finished, painting orders yet to be done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import sys
 import pygame
 from pygame import *
+import pygame.gfxdraw
 from diplomacy import Game
 from diplomacy.utils.export import to_saved_game_format
 
@@ -16,13 +17,13 @@ class VisualTerit:
             self.image = image.load(path)
 
 
-def draw_tinted(img, col):
+def draw_tinted(img, col, pos=(0,0)):
     tinted = img.convert_alpha()
     tinted.fill(col, None, BLEND_RGBA_MULT)
-    screen.blit(tinted, (0, 0))
+    screen.blit(tinted, pos)
 
 
-def adjust_influence():
+def set_starting_influence():
     game.get_power('AUSTRIA').influence.extend(['GAL', 'BOH', 'TYR'])
     game.get_power('ENGLAND').influence.extend(['CLY', 'YOR', 'WAL'])
     game.get_power('FRANCE').influence.extend(['BUR', 'GAS', 'PIC'])
@@ -32,10 +33,22 @@ def adjust_influence():
     game.get_power('TURKEY').influence.extend(['SYR', 'ARM'])
 
 
+def adjust_influence():
+    for i in range(0, len(power_names)):
+        cur_power = game.get_power(power_names[i])
+        for terit in cur_power.influence:
+            if terit in game.map.scs and terit not in cur_power.centers:
+                cur_power.influence.remove(terit)
+        for terit in cur_power.centers:
+            if terit not in cur_power.influence:
+                cur_power.influence.append(terit)
+
+
+
 # Creating a game
 # Alternatively, a map_name can be specified as an argument. e.g. Game(map_name='pure')
 game = Game()
-adjust_influence()
+set_starting_influence()
 pygame.font.init()  # for writing text on the map
 myfont = pygame.font.SysFont('arial', 30)
 
@@ -124,11 +137,11 @@ army_positions = {
     'ALB': (621, 665),
     'ANK': (877, 650),
     'APU': (577, 684),
-    'ARM': (947, 632),
+    'ARM': (964, 642),
     'BAL': (573, 357),
     'BAR': (773, 23),
     'BEL': (387, 424),
-    'BER': (519, 417),
+    'BER': (564, 393),
     'BLA': (849, 603),
     'BOH': (561, 495),
     'BOT': (635, 266),
@@ -138,11 +151,11 @@ army_positions = {
     'BUL/SC': (710, 672),
     'BUL': (719, 635),
     'BUR': (412, 495),
-    'CLY': (320, 271),
-    'CON': (770, 664),
+    'CLY': (324, 269),
+    'CON': (775, 685),
     'DEN': (502, 328),
     'EAS': (795, 808),
-    'EDI': (342, 271),
+    'EDI': (349, 281),
     'ENG': (300, 428),
     'FIN': (672, 213),
     'GAL': (688, 486),
@@ -158,7 +171,7 @@ army_positions = {
     'LVP': (330, 344),
     'LYO': (397, 624),
     'MAO': (61, 508),
-    'MAR': (376, 586),
+    'MAR': (371, 582),
     'MOS': (758, 364),
     'MUN': (491, 491),
     'NAF': (271, 772),
@@ -167,39 +180,39 @@ army_positions = {
     'NWY': (492, 249),
     'NTH': (410, 297),
     'NWG': (472, 34),
-    'PAR': (357, 493),
+    'PAR': (364, 489),
     'PIC': (358, 451),
     'PIE': (448, 571),
     'POR': (154, 597),
     'PRU': (617, 390),
-    'ROM': (504, 650),
-    'RUH': (0, 0),
-    'RUM': (0, 0),
-    'SER': (0, 0),
-    'SEV': (0, 0),
-    'SIL': (0, 0),
-    'SKA': (0, 0),
-    'SMY': (0, 0),
-    'SPA/NC': (0, 0),
-    'SPA/SC': (0, 0),
-    'SPA': (0, 0),
-    'STP/NC': (0, 0),
-    'STP/SC': (0, 0),
-    'STP': (0, 0),
-    'SWE': (0, 0),
-    'SYR': (0, 0),
-    'TRI': (0, 0),
-    'TUN': (0, 0),
-    'TUS': (0, 0),
-    'TYR': (0, 0),
-    'TYS': (0, 0),
-    'UKR': (0, 0),
-    'VEN': (0, 0),
-    'VIE': (0, 0),
-    'WAL': (0, 0),
-    'WAR': (0, 0),
-    'WES': (0, 0),
-    'YOR': (0, 0),
+    'ROM': (502, 645),
+    'RUH': (440, 458),
+    'RUM': (741, 592),
+    'SER': (646, 630),
+    'SEV': (843, 520),
+    'SIL': (567, 444),
+    'SKA': (506, 284),
+    'SMY': (827, 730),
+    'SPA/NC': (209, 554),
+    'SPA/SC': (240, 688),
+    'SPA': (250, 628),
+    'STP/NC': (780, 164),
+    'STP/SC': (725, 267),
+    'STP': (783, 237),
+    'SWE': (576, 263),
+    'SYR': (934, 766),
+    'TRI': (574, 610),
+    'TUN': (426, 774),
+    'TUS': (478, 617),
+    'TYR': (500, 535),
+    'TYS': (486, 694),
+    'UKR': (772, 466),
+    'VEN': (494, 584),
+    'VIE': (591, 540),
+    'WAL': (307, 378),
+    'WAR': (630, 437),
+    'WES': (299, 705),
+    'YOR': (347, 365),
 }
 
 # set canvas size variables
@@ -213,6 +226,8 @@ seaborders = image.load('seaborders.png')
 heightmap = image.load('heightmap.png')
 papertexture = image.load('newspaper2.png')
 forts = image.load('forts.png')
+shield = image.load('shield.png')
+arrow = image.load('arrow.png')
 
 # draw canvas
 screen = display.set_mode((width, height))
@@ -256,7 +271,7 @@ def paint_terits():
 
 def paint_troops():
     for i in range(0, len(power_names)):
-        print(game.get_power(power_names[i]).units)
+        # print(game.get_power(power_names[i]).units)
         for unit in game.get_power(power_names[i]).units:
             type = unit[0]
             position = army_positions.get(unit[2:])
@@ -280,19 +295,105 @@ def paint_map():
     display.update()
 
 
+def midpoint(p1, p2):
+    return (p1[0] + p2[0])//2, (p1[0] + p2[0])//2
+
+
+# def arrow(p1, p2, col):
+   # m1 = midpoint(p1, p2)
+   # pygame.gfxdraw.bezier(screen, (p1, m1, p2), 5, col)
+
+
+def trigon(p1, p2, col):
+    if p2[1] - p1[1] == 0:
+        pygame.gfxdraw.filled_trigon(p1[0], p1[1]+10, p1[0], p1[0]-10, p2[0], p2[1], (0, 0, 0, 255))
+        pygame.gfxdraw.filled_trigon(p1[0], p1[1]+7, p1[0], p1[0]-7, p2[0], p2[1], col)
+    if p2[0]-p1[0] == 0:
+        u = 0
+    else:
+        a = (p2[1]-p1[1])/(p2[0]-p1[0])
+        u = -1/a
+
+
+def draw_arrow_nieeeee(s, d):
+    w = 10
+    if d[0] == s[0]:
+        a = 'INF'
+        u = 0
+    elif d[1] == s[1]:
+        a = 0
+        u = 'INF'
+    else:
+        a = (d[1] - s[1])/(d[0] - s[0])
+        u = -1/a
+    print(w, " ", a, " ", u)
+
+
+def dir_v(s, d):
+    x = d[0] - s[0]
+    y = d[1] - s[1]
+    return x, y
+
+
+def interpolation(s, d, v):
+    direction = dir_v(s, d)
+    return v*direction[0], v*direction[1]
+
+
+def draw_arrow(s, d, col):
+    direction = dir_v(s, d)
+    udir = direction[1], -direction[0]
+    m1 = s[0]+0.75*direction[0] - 0.05*udir[0], s[1]+0.75*direction[1] -0.05*udir[1]
+    m2 = s[0]+0.75*direction[0] + 0.05*udir[0], s[1]+0.75*direction[1] +0.05*udir[1]
+    mcol = col[0], col[1], col[2], 180
+    pygame.gfxdraw.filled_polygon(screen, (s, m1, d, m2), (0, 0, 0, 255))
+    pygame.gfxdraw.filled_polygon(screen, (s, m1, d, m2), mcol)
+
+
+def paint_orders():
+    for i in range(0, len(power_names)):
+        for order in game.get_orders(power_names[i]):
+            # print(order)
+            sp = order.split(' ')
+            if sp[2] == 'H':
+                draw_tinted(shield, power_colors[i], army_positions.get(sp[1]))
+                # print("hold at ", sp[1], " ", army_positions.get(sp[1]))
+            elif sp[2] == '-':
+                # draw_tinted(arrow, power_colors[i], army_positions.get(sp[3]))
+                draw_arrow(army_positions.get(sp[1]), army_positions.get(sp[3]), power_colors[i])
+                # arrow(army_positions.get(sp[1]), army_positions.get(sp[3]), power_colors[i])
+            elif sp[2] == 'S':
+                mypos = army_positions.get(sp[1])
+                if len(sp) == 5:
+                    supported = army_positions.get(sp[3])
+                    # print(dir_v(mypos, supported))
+                    # draw_arrow(mypos, supported, power_colors[i])
+                    # draw_tinted(shield, power_colors[i], interpolation(mypos, supported, 0.5))
+                else:
+                    supported = army_positions.get(sp[4])
+                    # target = army_positions.get(sp[6])
+                    # draw_arrow(mypos, interpolation(supported, target, 0.7), power_colors[i])
+    display.update()
+
+
 while not game.is_game_done:
     paint_map()
     # Getting the list of possible orders for all locations
     possible_orders = game.get_all_possible_orders()
-    print(game.get_state())
-    print(game.get_all_possible_orders())
+    # print(game.get_state())
+    # print(game.get_all_possible_orders())
+    wait_for_any_key()
 
+    print(game.get_all_possible_orders())
+    print(game.build_caches())
     # For each power, randomly sampling a valid order
     for power_name, power in game.powers.items():
+        # print(game.get_orderable_locations(power_name))
         power_orders = [random.choice(possible_orders[loc]) for loc in game.get_orderable_locations(power_name)
                         if possible_orders[loc]]
         game.set_orders(power_name, power_orders)
 
+    paint_orders()
     # Messages can be sent locally with game.add_message
     # e.g. game.add_message(Message(sender='FRANCE',
     #                               recipient='ENGLAND',
@@ -303,8 +404,10 @@ while not game.is_game_done:
     # Processing the game to move to the next phase
     # print(game.get_orders('GERMANY'))
     print("next phase")
-    print(game.get_current_phase())
+    # print(game.get_current_phase())
     game.process()
+    adjust_influence()
+
     # print('Russian centres:', game.get_centers('RUSSIA'))
     # print(game.get_all_possible_orders())
     # print(game.get_units('GERMANY'))
